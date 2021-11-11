9678bbbd889c5a080acc90737084f620fb6b95fd
Alexandr <Formak21@ya.ru>
2021-11-11 17:51:33 +0300

    update

--- src/Client/gui.py
+++ src/Client/gui.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -9,7 +10,22 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('mainwindow.ui', self)
         self.setFixedSize(280, 210)
-        self.SendButton.
+        self.pushButton.clicked.connect(self.run)
+
+    def encode(self, text):
+        return
+
+    def decode(self, text):
+        return
+
+    def send(self):
+        name = self.NameLine.text()
+        text = self.TextLine.text()
+        time = datetime.datetime.now()
+        if len(name) > 0 and len(text) > 0:
+            pass
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
