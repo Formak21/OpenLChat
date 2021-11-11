8e14d7e75835297cfd7a0b0cdc16b969c73ff39f
Alexandr <Formak21@ya.ru>
2021-11-11 16:26:37 +0300

    ..

--- src/Client/main.py
+++ src/Client/main.py
@@ -1,11 +1,12 @@
 import socket
+import json
 
-Server = ("192.168.88.246", int(input('PORT:'))
-Message = input()
+Server = ("IP", int(input('PORT:')))
+Message = [input(), input(), input()]
 while True:
     con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     con.connect(Server)
-    con.sendall(Message.encode('utf-8'))
-    print(con.recv(1024).decode(utf-8))
-    Message = input()
+    con.sendall(json.dumps(Message).encode('utf-8'))
+    print(json.JSONDecoder().decode(con.recv(1024).decode('utf-8')))
+    Message = [input(), input(), input()]
     con.close()
--- src/Server/main.py
+++ src/Server/main.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 # getting server up
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -6,10 +7,10 @@ listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 Server = (socket.gethostbyname(socket.gethostname()), int(input('PORT:')))
 listener.bind(Server)
 listener.listen(1)
-
 while True:
     data = listener.accept()
-    text = data[0].recv(1024).decode("utf-8")
-    if text:
-        print(text)
-        data[0].sendall('SERVER_ANSWER'.encode("utf-8"))
+    my_data = data[0].recv(1024).decode('utf-8')
+    if my_data:
+        my_data = json.JSONDecoder().decode(my_data)
+        print(my_data)
+        data[0].sendall(json.dumps(['test']).encode("utf-8"))
