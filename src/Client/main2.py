92e28fa68b88d3fa25dd804956aa622c2d0bf922
Alexandr <Formak21@ya.ru>
2021-11-16 19:45:46 +0300

    TEST CLIENT

--- src/Client/main2.py
+++ src/Client/main2.py
@@ -1,9 +1,13 @@
+import datetime
 import socket
+import json
 
 Server = (input('IP:'), int(input('PORT:')))
 con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-con.connect(Server)
-con.send(input().encode('utf-8'))
-con.send(input().encode('utf-8'))
-con.send(input().encode('utf-8'))
-con.close()
+while True:
+    con.connect(Server)
+    con.send(json.dumps({'command': 'send', 'data': {'name': f'{input("NAME:")}', 'message': f'{input("MESSAGE:")}',
+                                                     'time': datetime.datetime.now().strftime(
+                                                         '%d.%m.%Y %H:%M:%S')}}).encode('utf-8'))
+    print(con.recv(500).decode('utf-8'))
+    con.close()
